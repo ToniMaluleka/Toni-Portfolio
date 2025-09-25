6474f597f97314f6708e9f8f940ad26917cda480
Hlayiseka <51748514+ToniMaluleka@users.noreply.github.com>
2025-09-25 07:59:48 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/toni-portfolio/", // MUST match GitHub repo name
-});
